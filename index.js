3c7a8f630240439f3f79ee0839bc297e49b0ee7b
Hoon Lee <yl.hoony@gmail.com>
2018-04-27 05:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const userName = ''
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
+
 function getIssues() {
 }
 
@@ -8,15 +12,29 @@ function createIssue() {
 }
 
 function showResults(json) {
+  console.log(json);
+
+
+
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'learn-co-curriculum/javascript-fetch-lab';
   //use fetch to fork it!
+  const token = getToken();
+
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'get',
+    headers: {
+      Authorization : `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+
+
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return 'c8335048e38f91a993efae27ca93af1c31571c2a'
 }
