a8e51dd95c5bdac4279fd18393822b11e8fb394a
Hoon Lee <yl.hoony@gmail.com>
2018-04-27 05:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const userName = ''
 const baseApi = 'https://api.github.com/'
 const fork = `${userName}/javascript-fetch-lab`
 
+function Issue(attributes){
+  this.title = attributes.title;
+  this.body = attributes.body;
+  this.url = attributes.url;
+}
+
+function Repo(attributes){
+  this.url = attributes.url;
+}
+
+Issue.prototype.template = function(){
+   var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
+   return template;
+};
+
+Repo.prototype.template = function(){
+  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+  return template;
+};
+//
+
+
+
+
 function getIssues(data) {
   fetch(`${baseApi}repos/${fork}/issues`).
     then(resp => {
